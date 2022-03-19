2d6c6b512b91b963531f30494471f98713c4bcc8
Mary Braunger <maryjezek@aol.com>
2022-03-18 21:01:57 -0400

    changes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -7,12 +7,12 @@ const {
   deleteUser,
 } = require("../../controllers/user-controller");
 
-// /api/pizzas
+// /api/users
 router.route("/")
 .get(getAllUsers)
 .post(createUser);
 
-// /api/pizzas/:id
+// /api/users/:id
 router.route("/:id")
 .get(getUserById)
 .put(updateUser)
